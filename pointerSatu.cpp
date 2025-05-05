023ac952f6a1df1fdf969e907820f624e4ade06f
mansoor <t.alyamani95@gmail.com>
2025-05-05 19:12:28 +0700

    pointer one  i will study it in home

---
+++ pointerSatu.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int a= 10;
+int b=6;
+int y=20;
+int *c=&a;
+cout<<"Alamat nemori : "<<c << endl;
+cout<<" Nilai dari c : "<<*c<<endl;
+c= &y;
+
+cout<<"Alamat nemori : "<<c << endl;
+cout<<" Nilai dari c : "<<*c<<endl;
+
+//refernce
+ int &d=b;
+ cout<<"Na;ai "<<d<<endl;
+ cout<<"Reference"<< &d<<endl;
+ int f=5;
+ int g=f;
+ 
+
+
+};
